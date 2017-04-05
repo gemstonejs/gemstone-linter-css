31aa9a775d721372ea00b4c2808c2dbae0ad7357
Ralf S. Engelschall <rse@engelschall.com>
2017-04-05 10:29:13 +0200

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# gemstone-linter-css
-Linter for CSS
+
+<img src="https://rawgit.com/gemstonejs/gemstone-artwork/master/gemstone-logo-white.svg" width="300" align="right" alt=""/>
+
+Gemstone Linter CSS
+===================
+
+<p/>
+<img src="https://nodei.co/npm/gemstone-linter-css.png?downloads=true&stars=true" alt=""/>
+<p/>
+<img src="https://david-dm.org/rse/gemstone-linter-css.png" alt=""/>
+
+About
+-----
+
+This is the Linter for CSS files in the
+[Gemstone JavaScript Technology Stack](http://gemstonejs.com).
+
+Copyright &amp; License
+-----------------------
+
+Copyright &copy; 2016-2017 [GemstoneJS Project](http://gemstonejs.com)<br/>
+Licensed under [Apache License 2.0](https://spdx.org/licenses/Apache-2.0)
+
---
+++ gemstone-linter-css.js
@@ -0,0 +1,107 @@
+/*
+**  GemstoneJS -- Gemstone JavaScript Technology Stack
+**  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+**  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
+*/
+
+/*  load external requirements  */
+const path                    = require("path")
+const fs                      = require("fs-promise")
+const clone                   = require("clone")
+const PostCSS                 = require("postcss")
+const PostCSSSCSS             = require("postcss-scss")
+const StyleLint               = require("stylelint")
+const GemstoneStylelintConfig = require("gemstone-config-stylelint")
+
+/*  exported API function  */
+module.exports = async function (filenames, opts = {}, report = { sources: {}, findings: [] }) {
+    let passed = true
+
+    /*  interate over all source files  */
+    if (typeof opts.progress === "function")
+        opts.progress(0.0, "linting CSS: starting")
+    for (let i = 0; i < filenames.length; i++) {
+        /*  indicate progress  */
+        if (typeof opts.progress === "function")
+            opts.progress(i / filenames.length, `linting CSS: ${filenames[i]}`)
+
+        /*  read source code  */
+        let source = await fs.readFile(filenames[i], "utf8")
+
+        /*  determine name of source  */
+        let name = path.relative(process.cwd(), filenames[i])
+
+        /*  determine StyleLint configuration  */
+        let config = clone(GemstoneStylelintConfig)
+        if (typeof opts.rules === "object")
+            Object.assign(config.rules, opts.rules)
+
+        /*  lint CSS via PostCSS/SCSS/StyleLint  */
+        let lintResult = await PostCSS([
+            StyleLint({ config: config })
+        ]).process(source, {
+            from:   name,
+            to:     name,
+            parser: PostCSSSCSS
+        }).catch((err) => {
+            /*  take CSS parsing errors  */
+            if (typeof err === "object" && err.name === "CssSyntaxError") {
+                /*  transform CssSyntaxError  */
+                let [ ,, line, col, msg ] = err.message.match(/^(.+):(\d+):(\d+):\s*(.+)$/)
+                line = parseInt(line)
+                col  = parseInt(col)
+                report.findings.push({
+                    ctx:      "CSS",
+                    filename: name,
+                    line:     line,
+                    column:   col,
+                    message:  msg,
+                    ruleProc: "postcss-scss",
+                    ruleId:   "*"
+                })
+            }
+            else {
+                /*  Ops, any other unknown error?  */
+                report.findings.push({
+                    ctx:      "CSS",
+                    filename: name,
+                    line:     1,
+                    column:   1,
+                    message:  `UNKNOWN PARSING ERROR: ${err}`,
+                    ruleProc: "postcss-scss",
+                    ruleId:   "*"
+                })
+            }
+            report.sources[name] = source
+            passed = false
+        })
+
+        /*  take CSS styling errors  */
+        if (typeof lintResult === "object" && lintResult.messages) {
+            lintResult.messages.forEach((message) => {
+                let msg = message.text
+                let ruleId = "unknown"
+                let m = msg.match(/^(.+)\s*\((.+)\)$/)
+                if (m !== null) {
+                    msg    = m[1]
+                    ruleId = m[2]
+                }
+                report.findings.push({
+                    ctx:      "CSS",
+                    filename: name,
+                    line:     message.line,
+                    column:   message.column,
+                    message:  msg,
+                    ruleProc: "stylelint",
+                    ruleId:   ruleId
+                })
+                report.sources[name] = source
+                passed = false
+            })
+        }
+    }
+    if (typeof opts.progress === "function")
+        opts.progress(1.0, "y")
+    return passed
+}
+
